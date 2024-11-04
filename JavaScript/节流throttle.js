6eabf0e9f14955be2e5d506d38bb4cf087813411
zjc <2782171149@qq.com>
2024-11-05 00:05:25 +0800

    feat: 弄清楚了节流中fn.apply(this, arguments)和fn()的区别，并成功通过demo来验证猜想；

--- JavaScript/节流throttle.js
+++ JavaScript/节流throttle.js
@@ -1,11 +1,41 @@
+let count = 0;
+
 function throttle (fn, wait) {
   let prev = new Date();
+  const that1 = this;
+  // console.log("throttle外", this);
   return function () {
+    // const that2 = this;
+    // console.log("throttle内", this);
+    // console.log("是否等", that1 === that2);
+
     let now = new Date();
     // 如果超出了需等待的 wait，那就可以执行啦总算
     if (now - prev > wait) {
-      fn.apply(this, arguments);
+      fn();
+      // fn.apply(this, arguments);
       prev = new Date();
     }
   }
 }
+
+// const throttleTest = throttle(test, 2000);
+const obj = {
+  a: 1,
+  throttleTestObj: throttle(test, 2000) // 绑定在了obj内
+}
+
+const throttleTestWindow = throttle(test, 2000); // 绑定在了window
+
+
+function test () {
+  count++;
+  console.log("打印this环境", this, count);
+
+};
+
+
+
+setInterval(() => {
+  obj.throttleTestObj();
+}, 500)
