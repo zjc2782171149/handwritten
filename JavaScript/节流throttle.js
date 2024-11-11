835bb0099c3469044554e7c97c6b2be1439f1b4d
zjc <2782171149@qq.com>
2024-11-11 23:36:59 +0800

    feat: 优化了防抖和节流的书写

--- JavaScript/节流throttle.js
+++ JavaScript/节流throttle.js
@@ -3,12 +3,7 @@ let count = 0;
 function throttle (fn, wait) {
   let prev = new Date();
   const that1 = this;
-  // console.log("throttle外", this);
   return function () {
-    // const that2 = this;
-    // console.log("throttle内", this);
-    // console.log("是否等", that1 === that2);
-
     let now = new Date();
     // 如果超出了需等待的 wait，那就可以执行啦总算
     if (now - prev > wait) {
--- JavaScript/防抖debounce.js
+++ JavaScript/防抖debounce.js
@@ -6,6 +6,8 @@ function debounce (fn, wait, immediate) {
     if (timer) clearTimeout(timer); // 如果debounce不再触发，那么 setTimeout 过 wait 会自动执行，但如果再次触发了就要清空计时器，重新计时
     timer = setTimeout(() => {
       fn.apply(this, arguments);
+      timer && clearTimeout(timer);
+      timer = null;
     }, wait);
   }
 }
