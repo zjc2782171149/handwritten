adbc29d1e9c94465f5b626fb473dea13a9d78167
zjc <2782171149@qq.com>
2024-11-15 21:56:56 +0800

    feat: 优化倒数计时器、实现interface的部分继承、实现关键词数组表示完整字符串算法

--- JavaScript/倒数计时器.js
+++ JavaScript/倒数计时器.js
@@ -1,28 +1,35 @@
-function time (num) {
-  if (num > 86399000) {
-    num = 86399000;
-  }
-  let initTime = num;
-
-  function showCurrentTime (initTime) {
-    const totalSeconds = Math.floor(initTime / 1000);
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
-    const formattedTime =
-      String(hours).padStart(2, '0') + ':' +
-      String(minutes).padStart(2, '0') + ':' +
-      String(seconds).padStart(2, '0');
-    console.log(formattedTime);
+function translateTime (num) {
+  if (num < 0 || num > 86399) {
+    console.log("数值无效，过大或过小"); // 23:59:59 是 max，当然有部分地方要求可以 24:00:00
+    return;
   }
 
-  showCurrentTime(initTime);
-  const timer = setInterval(() => {
-    initTime -= 1000;
-    if (initTime < 0) {
-      clearInterval(timer);
-      return;
-    }
-    showCurrentTime(initTime);
-  }, 1000);
+  // 传进来的是秒数，比如100就是100秒
+  const oneMin = 60; // 1分钟是60秒
+  const oneHour = 60 * 60; // 1小时是60分钟，即 60 * 60秒
+
+  const nowHour = Math.floor(num / oneHour); // 计算小时数
+  const nowMin = Math.floor((num % oneHour) / oneMin); // 计算分钟数
+  const nowSecond = num % oneMin; // 计算秒数
+
+  return `${nowHour >= 10 ? nowHour : '0' + nowHour}:${nowMin >= 10 ? nowMin : '0' + nowMin}:${nowSecond >= 10 ? nowSecond : '0' + nowSecond}`;
 }
+
+let count = 0; // 循环10次后默认给关闭，业务需求一般是有个按钮来暂停倒计时，这里只是模拟暂停
+let currentTime = 100; // 时间，即秒数
+
+const timeInterval = setInterval(() => {
+  console.log(translateTime(currentTime));
+  // 打印完最后一个0秒后，就停止计时器
+  if (currentTime === 0) {
+    timeInterval && clearInterval(timeInterval);
+  } else {
+    currentTime -= 1;
+  }
+
+  count++;
+  // 循环10次后关闭
+  if (count >= 10) {
+    timeInterval && clearInterval(timeInterval);
+  }
+}, 1000);
--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # handwritten
-2024.10.30 Shopee Infra 一面
-四道算法/场景
-1. 类似this指向判断结果
-2. Promise.retry 的实现
-3. 无限次 curry 的升级，但最后通过 .count 才结束
-4. 类似深拷贝的场景题
+
+> 记录平日遇到的手写代码题，每天多看看进行复习！
